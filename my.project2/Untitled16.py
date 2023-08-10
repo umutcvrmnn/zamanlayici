4dbc30630af7fdf2e08d57c00ead51b87f5fcb9a
umutcvrmnn <141020456+umutcvrmnn@users.noreply.github.com>
2023-08-10 15:54:38 +0300

    Create Untitled16.py
    zamanlayici

---
+++ my.project2/Untitled16.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# coding=utf-8
+
+"""
+    Zamanlayici kullanimi
+    Tarihi gösteren küçük bir uygulama
+"""
+
+import time
+import os
+import platform
+
+
+def temizle():
+    if platform.system() == 'Linux' or platform.system() == 'Darwin':
+        os.system('clear')
+    else:
+        os.system('cls')
+
+
+while True:
+
+    zaman = time.localtime()
+    yil = zaman[0]
+    ay = zaman[1]
+    gun = zaman[2]
+    saat = zaman[3]
+    dakika = zaman[4]
+    saniye = zaman[5]
+
+    time.sleep(1)
+    temizle()
+
+    print("""
+        tarih: {}/{}/{}
+        saat : {}:{}:{}
+        """.format(gun, ay, yil, saat, dakika, saniye))
+
